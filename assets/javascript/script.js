223ea7ac4ca4694e6beca2cae16a127c6747ce7c
Anthony Gonzalez <shibby75@live.com>
2021-11-19 22:34:38 -0700

    added to js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -39,5 +39,78 @@ var displayWeather = function (weather, citySearch) {
   cityCurrentWeather.textContent = "";
   searchedCity.textContent = citySearch;
 
-  console.log(weather);
+  var currentDate = document.createElement("span");
+  currentDate.textContent =
+    " (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+  citySearchInputEl.appendChild(currentDate);
+
+  var weatherArt = document.createElement("img");
+  weatherArt.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+  );
+  citySearchInputEl.appendChild(weatherArt);
+
+  var tempInfo = document.createElement("span");
+  tempInfo.textContent = "Temperature: " + weather.main.temp + " °F";
+  tempInfo.classList = "list-group-item";
+
+  cityCurrentWeather.appendChild(tempInfo);
+
+  var humidityInfo = document.createElement("span");
+  humidityInfo.textContent = "Humidity: " + weather.main.humidity + " %";
+  humidityInfo.classList = "list-group-item";
+
+  cityCurrentWeather.appendChild(humidityInfo);
+
+  var windSpeed = document.createElement("span");
+  windSpeed.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+  windSpeed.classList = "list-group-item";
+
+  cityCurrentWeather.appendChild(windSpeed);
+
+  uvIndexInfo(latitude, longitude);
+};
+
+var latitude = weather.coord.lat;
+var longitude = weather.coord.lon;
+
+var uvIndexInfo = function (latitude, longitude) {
+  var apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+  fetch(apiURL).then(function (response) {
+    response.json().then(function (data) {
+      displayUvIndexInfo(data);
+    });
+  });
+};
+
+var displayUvIndexInfo = function (index) {
+  var uvIndex = document.createElement("div");
+  uvIndex.textContent = "UV Index: ";
+  uvIndex.classList = "list-group-item";
+
+  uvIndexValue = document.createElement("span");
+  uvIndexValue.textContent = index.value;
+
+  if (index.value <= 2) {
+    uvIndexValue.classList = "favorable";
+  } else if (index.value > 2 && index.value <= 8) {
+    uvIndexValue.classList = "moderate";
+  } else if (index.value > 8) {
+    uvIndexValue.classList = "severe";
+  }
+
+  uvIndex.appendChild(uvIndexValue);
+
+  cityCurrentWeather.appendChild(uvIndex);
+};
+
+var fiveDays = function (city) {
+  var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
+
+  fetch(apiURL).then(function (response) {
+    response.json().then(function (data) {
+      displayFiveDays(data);
+    });
+  });
 };
