5c9930953af6d0aa2a5accc548d2933d88256c39
Anthony Gonzalez <shibby75@live.com>
2021-11-19 17:33:21 -0700

    added more html and started js

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+var cities = [];
+
+var citySearch = document.getElementById("city-search");
+var cityInput = document.getElementById("city-input");
+var pastSearches = document.getElementById("past-searches");
+var currentForecast = document.getElementById("current-forecast");
+var searchedCity = document.getElementById("searched-city");
