d06549ac6e94ed9be2738db4e78fd1b229a8ee01
Anthony Gonzalez <shibby75@live.com>
2021-11-19 18:05:24 -0700

    moved script tags in html, added submit function in js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,31 @@ var cityInput = document.getElementById("city-input");
 var pastSearches = document.getElementById("past-searches");
 var currentForecast = document.getElementById("current-forecast");
 var searchedCity = document.getElementById("searched-city");
+var cityCurrentWeather = document.getElementById("city-current-weather");
+var fiveDayForecast = document.getElementById("five-day-forecast");
+var forecast = document.getElementById("forecast");
+var fiveDayContainer = document.getElementById("five-day-container");
+
+var getCityWeather = function (city) {
+  var apiKey = "bf57feecdaa505ef2a8d3e03515ca737";
+  var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+  fetch(apiURL).then(function (response) {
+    response.json().then(function (data) {
+      displayWeather(data, city);
+    });
+  });
+};
+
+var formSubmit = function (event) {
+  event.preventDefault();
+  var city = cityInput.ariaValueMax.trim();
+  if (city) {
+    getCityWeather(city);
+    get5day(city);
+    cities.unshift({ city });
+    cityInput.value = "";
+  } else {
+    alert("Please enter a city to search!");
+  }
+};
